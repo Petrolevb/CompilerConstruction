45ca0d84f30769c4f696a7bbc0644c03bd5020c6
Petrolevb-KaliVirt <Vincebellec@gmail.com>
2013-04-24 11:31:22 +0200

    Debut generation code

---
+++ TypeCheck/Generator.hs
@@ -0,0 +1,21 @@
+module Generator where
+
+
+import AnnotatedAbs
+
+
+
+generation :: AnnotatedProgram -> IO ()
+generation = undefined
+
+genTopDef :: AnnotatedTopDef -> IO ()
+genTopDef = undefined
+
+genBlock :: AnnotatedBlock -> IO ()
+genBlock = undefined
+
+genStmt :: AnnotatedStmt -> IO ()
+genStmt = undefined
+
+genExp :: AnnotatedExp -> IO ()
+genExp = undefined
