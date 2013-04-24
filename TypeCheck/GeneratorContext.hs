e67808f59e6358e4f0d06f444ae2a9ef03e0c2e2
Petrolevb-KaliVirt <Vincebellec@gmail.com>
2013-04-24 13:52:22 +0200

    Amelioration contexte

--- TypeCheck/GeneratorContext.hs
+++ TypeCheck/GeneratorContext.hs
@@ -3,15 +3,29 @@ module GeneratorContext where
 import AnnotatedAbs as TYP
 import AbsJavalette as ABS
 
-type GenContext = String
+
+-- Context = (NameCurrentFunction, CounterLabel, [Variables, memory]
+type GenContext = (Ident, Integer, MapVars)
+type MapVars = [(Ident, Integer)]
+
+newMap :: MapVars
+newMap = []
+
+newContext :: GenContext
+newContext = ((Ident ""), 0, newMap)
 
 getMemory :: GenContext -> Ident -> Integer
-getMemory = undefined
+getMemory (_, _, mv)= undefined
 
 getNameFunc :: GenContext -> String
-getNameFunc = undefined
+getNameFunc ((Ident func), _, _) = func
 
 
 getLabel :: GenContext -> String
-getLabel = undefined
+getLabel ((Ident f), c, _) = f ++ "_" ++ show c
 -- get a label with the name of the current function and a counter
+incrLabelEnv :: GenContext -> GenContext
+incrLabelEnv (f, c, mv) = (f, c+1, mv)
+
+addFunc :: GenContext -> Ident -> GenContext
+addFunc (_, c, mv)  func = (func, c, mv)
