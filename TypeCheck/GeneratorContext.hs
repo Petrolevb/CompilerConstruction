b16013d3ff87897c9affd1cf7bf626cc5c781244
Wambou <lewambou@gmail.com>
2013-04-26 19:16:19 +0200

    Modification de l'env : ajout d'une pile de label

--- TypeCheck/GeneratorContext.hs
+++ TypeCheck/GeneratorContext.hs
@@ -4,44 +4,56 @@ import AnnotatedAbs as TYP
 import AbsJavalette as ABS
 
 
--- Context = (NameCurrentFunction, CounterLabel, [Variables, memory]
-type GenContext = (Ident, Integer, MapVars)
+-- Context = (NameCurrentFunction, CounterLabel, [Variables, memory], [StackLabel])
+type GenContext = (Ident, Integer, MapVars, [String])
 type MapVars = [(Type, Ident, Int)]
 
 newMap :: MapVars
 newMap = []
 
 newContext :: GenContext
-newContext = ((Ident ""), 0, newMap)
+newContext = ((Ident ""), 0, newMap, [])
 
 getMemory :: GenContext -> Ident -> (Type, Int)
-getMemory (_, _, mv) = getVar mv
+getMemory (_, _, mv, _) = getVar mv
 
 getVar :: MapVars -> Ident -> (Type, Int)
 getVar ((t, id, i):maps) search | id == search = (t, i)
                                 | otherwise = getVar maps search
 
 addVar :: GenContext -> (Type, Ident) -> GenContext
-addVar (id, c, mv) (typ, ident) = (id, c, (addInMv mv typ ident))
+addVar (id, c, mv, st) (typ, ident) = (id, c, (addInMv mv typ ident), st)
     where addInMv mv t i = mv ++ [(t, i, (length mv))]
 
 getNameFunc :: GenContext -> String
-getNameFunc ((Ident func), _, _) = func
+getNameFunc ((Ident func), _, _, _) = func
 
 
 getLabel :: GenContext -> String
-getLabel ((Ident f), c, _) = f ++ "_" ++ show c
+getLabel ((Ident f), c, _, _) = f ++ "_" ++ show c
 -- get a label with the name of the current function and a counter
 incrLabel :: GenContext -> GenContext
-incrLabel (f, c, mv) = (f, c+1, mv)
+incrLabel (f, c, mv, st) = (f, c+1, mv, st)
+
+-- add a label on the stack of label
+pushLabel :: GenContext -> String -> GenContext
+pushLabel (id, c, mv, st) label = (id, c, mv, (label:st))
+
+-- take the first label on the stack
+stackLabel :: GenContext -> String
+stackLabel (_, _, _, (label:st)) = label
+
+-- withdraw the top label from the stack
+popLabel :: GenContext -> GenContext
+popLabel(id, c, mv, (l:st)) = (id, c, mv, st)
 
 
 
 addFunc :: GenContext -> Ident -> GenContext
-addFunc (_, c, mv)  func = (func, c, mv)
+addFunc (_, c, mv, st)  func = (func, c, mv, st)
 
 addArgs :: GenContext -> [Arg] -> GenContext
-addArgs (f, c, mv) args = (f, c, (mapArgs mv 1 args))
+addArgs (f, c, mv, st) args = (f, c, (mapArgs mv 1 args), st)
 
 mapArgs :: MapVars -> Int -> [Arg] -> MapVars
 mapArgs mv _ [] = mv
