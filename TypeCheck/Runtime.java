bc1df02cb438dc29f6c3a2137a13bdc890741f02
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2013-04-26 16:27:45 +0200

    Add Runtime

---
+++ TypeCheck/Runtime.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Runtime
+{
+    static void printString(String s) { System.out.println(s); }
+    static void printInt(int i) { System.out.println(i); }
+    static void printDouble(double d) { System.out.println(d); }
+    static int readInt() { return scanner.nextInt(); }
+    static double readDouble() { return scanner.nextDouble(); }
+    
+    static Scanner scanner = new Scanner(System.in);
+
+}
