4f5635dcaa12935467211544c8fa5dfc3fa4efb3
Petrolevb-KaliVirt <Vincebellec@gmail.com>
2013-05-20 10:57:09 +0200

    Correction du contexte

--- jlc/src/LlvmContext.hs
+++ jlc/src/LlvmContext.hs
@@ -4,8 +4,8 @@ import AnnotatedAbs as TYP
 import AbsJavalette as ABS
 
 
--- Context = (NameCurrentFunction, CounterLabel, [Javalette_var, Llvm_name], [name, content], [StackLabel])
-type LlvmContext = (Ident, Integer, MapVars, MapStrs, [String])
+-- Context = (NameCurrentFunction, CounterVariable, [Javalette_var, Llvm_name], [name, content], [StackLabel])
+type LlvmContext = (Ident, Int, MapVars, MapStrs, [String])
 type MapVars = [(Ident, String)]
 type MapStrs = [(String, String)]
 
@@ -20,31 +20,31 @@ newContext = ((Ident ""), 0, newMapVars, newMapStrs, [])
 
 
 getMemory :: LlvmContext -> Ident -> String
-getMemory (_, _, mv, _) = getVar mv
+getMemory (_, _, mv, _, _) = getVar mv
 
-getVar :: MapVars -> Ident -> (Type, Int)
+getVar :: MapVars -> Ident -> String
 getVar mv id = case lookup id mv of
-                    Just x -> Just x
+                    Just x -> x
                     _      -> fail "Variable not found"
 
 -- Add the javalette ident to the lookup table with a new name
 addVar :: LlvmContext -> Ident -> LlvmContext
-addVar (id, c, mv, st) (Ident i) = (id, c+1, (addInMv mv i c), st)
-    where addInMv mv i c = mv ++ [(Ident i, (show i)++c)]
+addVar (id, c, mv, ms, st) (Ident i) = (id, c+1, (addInMv mv i c), ms, st)
+    where addInMv mv i c = mv ++ [(Ident i, i ++ (show c))]
 
 
 getNameFunc :: LlvmContext -> String
-getNameFunc ((Ident func), _, _, _) = func
+getNameFunc ((Ident func), _, _, _, _) = func
 
 addFunc :: LlvmContext -> Ident -> LlvmContext
-addFunc (_, c, mv, st)  func = (func, c, mv, st)
+addFunc (_, c, mv, ms, st)  func = (func, c, mv, ms, st)
 
 addArgs :: LlvmContext -> [Arg] -> LlvmContext
-addArgs (f, c, mv, st) args = (f, c+(length args), (mapArgs mv c args), st)
+addArgs (f, c, mv, ms, st) args = (f, c+(length args), (mapArgs mv c args), ms, st)
 
 mapArgs :: MapVars -> Int -> [Arg] -> MapVars
 mapArgs mv _ [] = mv
-mapArgs mv c (Arg typeA (Ident i):args) = mapArgs (mv ++ [(Ident i, (show i)++c)]) (c+1) args
+mapArgs mv c (Arg typeA (Ident i):args) = mapArgs (mv ++ [(Ident i, i++ (show c))]) (c+1) args
 
 {-
 getLabel :: LlvmContext -> String
